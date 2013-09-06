d89966cd670c97a5b14dff9cae76aceadcb72e77
gustavo <gustavo@memento>
2013-09-05 21:25:54 -0300

    cadastro de cheque

--- src/sisloc/controller/ChequeController.java
+++ src/sisloc/controller/ChequeController.java
@@ -1,7 +1,9 @@
 package sisloc.controller;
 
 import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
 
+@Resource
 public class ChequeController {
 	
 	@Path("/cheque/cadastrarcheque")
