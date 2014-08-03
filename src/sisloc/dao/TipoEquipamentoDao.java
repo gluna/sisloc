5193dc941f1bd4678c34a06cfa72ef9ba5799c11
gustavo <gustavo@memento>
2014-08-03 11:34:38 -0300

    mudanca de nome da pasta tipo para tipoequipamentos

--- src/sisloc/dao/TipoEquipamentoDao.java
+++ src/sisloc/dao/TipoEquipamentoDao.java
@@ -1,5 +1,65 @@
 package sisloc.dao;
 
-public class TipoEquipamentoDao {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import sisloc.modelo.TipoEquipamento;
+import br.com.caelum.vraptor.ioc.Component;
+
+@Component
+public class TipoEquipamentoDao {
+	
+	public EntityManager manager;
+
+	public TipoEquipamentoDao(EntityManager manager){
+		 this.manager = manager;
+	}
+	
+	public boolean salvar(TipoEquipamento tipoequipamento){
+		try{
+			manager.persist(tipoequipamento);
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean excluir(TipoEquipamento tipoequipamento){
+		try{
+			manager.remove(tipoequipamento);
+		}catch(Exception e){
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean atualizar(TipoEquipamento tipoequipamento){
+		try{
+			manager.merge(tipoequipamento);
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+			return false;
+		}
+		return true;
+	}
+	
+	public TipoEquipamento selectById(TipoEquipamento tipoequipamento) {
+		Query q = manager.createQuery("from TipoEquipamento t where t.id = :pid");
+		q.setParameter("pid", tipoequipamento.getId());
+		 
+		return (TipoEquipamento)q.getSingleResult();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<TipoEquipamento> listaTodos(){
+		List<TipoEquipamento> t;
+		
+		Query q = manager.createQuery("from TipoEquipamento order by descricao");
+		t = (List<TipoEquipamento>) q.getResultList();		
+		return t;
+	}
+	
 }
