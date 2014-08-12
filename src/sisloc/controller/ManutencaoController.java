2a652fee3028cd4e9192caa77c8a19daec678b0e
gustavo <gustavo@memento>
2014-08-11 21:24:06 -0300

    salvar manutencao com diminuicao das pecas

--- src/sisloc/controller/ManutencaoController.java
+++ src/sisloc/controller/ManutencaoController.java
@@ -2,10 +2,13 @@ package sisloc.controller;
 
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 import sisloc.dao.ManutencaoDao;
 import sisloc.dao.PecaDao;
 import sisloc.modelo.Manutencao;
 import sisloc.modelo.Peca;
+import sisloc.modelo.PecaManutencao;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
@@ -34,12 +37,37 @@ public class ManutencaoController {
 	public void salvar(Manutencao manutencao){
 		try {
 		     if(manutencao != null) {
+		    	 boolean ok = true;
+		    	 if(manutencao.getPecas() == null){
+		    		 JOptionPane.showMessageDialog(null,"verifique os itens");
+		    	 }else{
+		    		 for(PecaManutencao pm : manutencao.getPecas()){//verifica se a quantidade existe
+		    			 Peca p = pecaDao.selectById(pm.getPeca());
+		    			 if(p.getQuantidade() < pm.getQuantidade()){
+		    				 ok = false;
+		    			 }
+		    		 }
+		    		 
+		    		 if(ok){
+		    			 Double valor = 0.0;
+			    		 for(PecaManutencao pm : manutencao.getPecas()){
+			    			 Peca p = pecaDao.selectById(pm.getPeca());
+			    			 p.setQuantidade(p.getQuantidade()-pm.getQuantidade());
+			    			 valor = valor+(p.getValor()*pm.getQuantidade());
+			    		 }
+		    			 manutencao.setCusto(valor+"");
+		    		 }else{
+		    			 JOptionPane.showMessageDialog(null,"verifique as quantidades");
+		    		 }
+		    	 }
+		    	 
 		    	 if(manutencao.getId() == null) {
 		    		 dao.salvar(manutencao);
 		    	 } else {
 		    		 dao.atualizar(manutencao);
 	             }
 	          }
+		     result.include("manutencao", manutencao);
 		     result.redirectTo(this.getClass()).cadastrar();
 		} catch (Exception e) {
 			result.include("msg", e.getMessage());
