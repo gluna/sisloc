84f01565ff447b8563641a81e8e50f61cc1f2572
gustavo <gustavo@memento>
2014-09-06 18:36:49 -0300

    ajuste no orcamento

--- src/sisloc/controller/OrcamentosController.java
+++ src/sisloc/controller/OrcamentosController.java
@@ -1,38 +1,59 @@
 package sisloc.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import sisloc.dao.ClienteDao;
+import sisloc.dao.EquipamentoDao;
+import sisloc.dao.OrcamentoDao;
+import sisloc.modelo.Cliente;
+import sisloc.modelo.Equipamento;
+import sisloc.modelo.Orcamento;
+import sisloc.util.SislocUtils;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.view.Results;
 
 
 @Resource
 public class OrcamentosController {
 
-	/*private OrcamentoDao dao;
+	private OrcamentoDao dao;
 	private Result result;
 	private ServletContext context;
-	private ProdutoDao produtodao;
 	private ClienteDao clientedao;
+	private EquipamentoDao equipamentodao;
 	private HttpServletResponse response;
 	
 	public OrcamentosController(OrcamentoDao dao, Result result,final ServletContext context, 
-			ClienteDao clientedao, ProdutoDao produtodao, HttpServletResponse response){
+			ClienteDao clientedao, HttpServletResponse response, EquipamentoDao equipamentodao){
 		this.dao = dao;
 		this.result = result;
 		this.context = context;
-		this.produtodao = produtodao;
 		this.clientedao = clientedao;
 		this.response = response;
+		this.equipamentodao = equipamentodao;
 	}
 	
 	@Path("/orcamentos/cadastrar")
-	public List<Produto> cadastrar(){
-		List<Produto> t = produtodao.listaTodos();
+	public List<Equipamento> cadastrar(){
+		List<Equipamento> t = equipamentodao.listaTodos();
 		return t;
 	}
 	
 	@Post
 	@Path("/orcamentos/salvar")
 	public void salvar(Orcamento orcamento){
-		try {
+		/*try {
 		     if(orcamento != null) {
 		    	 for(OrcamentoDetalhe od : orcamento.getOrcamentodetalhe() ){
 		    		 od.setProduto(produtodao.selectById(od.getProduto()));
@@ -48,32 +69,32 @@ public class OrcamentosController {
 		     result.redirectTo(this.getClass()).cadastrar();
 		} catch (Exception e) {
 			result.include("msg", e.getMessage());
-		}
+		}*/
 	}
 	
 	@Path("/orcamentos/editar/{orcamento.id}")
 	public void editar(Orcamento orcamento){
-		orcamento = dao.selectById(orcamento);
+		/*orcamento = dao.selectById(orcamento);
 	    if(orcamento != null) {
 	         result.include("orcamento", orcamento);
 	    }
-	    result.redirectTo(this.getClass()).cadastrar();
+	    result.redirectTo(this.getClass()).cadastrar();*/
 	}
 	
 	@Path("/orcamentos/visualizar/{orcamento.id}")
 	public void visualizar(Orcamento orcamento){
-		orcamento = dao.selectById(orcamento);
+		/*orcamento = dao.selectById(orcamento);
 	    if(orcamento != null) {
 	         result.include("orcamento", orcamento);
 	    }
-	    //result.redirectTo(this.getClass()).cadastrar();
+	    //result.redirectTo(this.getClass()).cadastrar();*/
 	}
 	
 	@Path("/orcamentos/excluir/{orcamento.id}")
 	public void excluir(Orcamento orcamento){
-		orcamento = dao.selectById(orcamento);
+		/*orcamento = dao.selectById(orcamento);
 	    dao.excluir(orcamento);
-	    //result.redirectTo(this.getClass()).consultar();	
+	    //result.redirectTo(this.getClass()).consultar();*/	
 	}
 	
 	@Path("/orcamentos/consultar")
@@ -114,18 +135,18 @@ public class OrcamentosController {
     	result.permanentlyRedirectTo(this.getClass()).cadastrar();
 	}
 	
-	@Path("/orcamentos/getprecos/{produto.id}")
+	/*@Path("/orcamentos/getprecos/{produto.id}")
 	public void getprecos(Long p){
 		Produto prod = new Produto();
 		prod.setId(p);
 		List<Preco> precos = produtodao.selectById(prod).getPrecos();
 		result.use(Results.json()).withoutRoot().from(precos).serialize();  
-	}
+	}*/
 	
 	@Path("/orcamentos/consultaorcamento")
 	public List<Orcamento> consultaorcamento(Orcamento orcamento){
 		List<Orcamento> t = dao.consultaorcamento(orcamento);
 		return t;
-	}*/
+	}
 	
 }
